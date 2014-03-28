68aa4c373b2caf081b5d820796425dcb455b3b24
kremnik <kremnikov@gmail.com>
2014-03-28 05:18:49 +0400

    tab bug fixed

--- source.cpp
+++ source.cpp
@@ -1,10 +1,18 @@
 void main() {
-   z = 1;
-   do { 
-      x = x + 1;
-   } 
-   while (x == 1);
-   if (x == 1) {
-      y = 1;
-   }
+	z = 1;
+	do { 
+		x = x + 1; 
+		y;
+	} 
+	while (x == 1);
+	if (x == 1) {
+		y = 1;
+		if (y == 1) {
+			x = 1;
+			do {
+				y = x;
+			}
+			while (x == 1);
+		}
+	}
 }
